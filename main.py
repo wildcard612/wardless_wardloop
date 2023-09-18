fd70998d48f1a8c0b2f21fadf838e147eddd7247
WildCard <sebastian.okoniewski90@gmail.com>
2023-09-18 13:00:37 +0700

    calculated, and its bs

--- main.py
+++ main.py
@@ -82,10 +82,31 @@ def recoup(dmg, relife, max_life):
 # print("First tick:", ticks[0])
 # print("Last tick:", ticks[-1])
 # print("Scaled last tick to 1s:", ticks[-1] / 0.033)
+def compute_ticks_degen(damage_interval=0.198, damage_amount=2480, total_duration=3.0, tick_duration=0.033, progenesis = 37, petri = 40):
+    total_recoup_from_one_hit = (damage_amount * (1 - progenesis / 100)) * (1 - petri * 1.41 / 100 )
+    num_ticks = int(total_duration / tick_duration)
+
+    ticks_degen = []
+    for tick in range(1, num_ticks + 1):
+        current_time = tick * tick_duration
+        total_recoup_this_tick = 0
+
+        # Przeszukujemy wszystkie serie obrażeń zadane w ciągu ostatnich 3 sekund
+        for time_since_last_hit in range(0, int(total_duration/damage_interval) + 1):
+            if 0 < current_time - time_since_last_hit * damage_interval <= 3:
+                total_recoup_this_tick += 2 * tick_duration * (current_time - time_since_last_hit * damage_interval) * total_recoup_from_one_hit / (total_duration ** 2)
+
+        ticks_degen.append(round(total_recoup_this_tick, 5))
+
+    return ticks_degen
+
+ticks_degen = compute_ticks_degen()
+print("First tick degen:", ticks_degen[0])
+print("Last tick degen:", ticks_degen[-1])
+print("Scaled last tick degen to 1s:", ticks_degen[-1] / 0.033)
 
 
-
-def compute_ticks(damage_interval=0.198, damage_amount=3000, recoup_percentage=1, total_duration=3.0, tick_duration=0.033):
+def compute_ticks(damage_interval=0.198, damage_amount=2480, recoup_percentage=1.2, total_duration=3.0, tick_duration=0.033):
     total_recoup_from_one_hit = damage_amount * recoup_percentage
     num_ticks = int(total_duration / tick_duration)
 
@@ -108,25 +129,35 @@ print("First tick:", ticks[0])
 print("Last tick:", ticks[-1])
 print("Scaled last tick to 1s:", ticks[-1] / 0.033)
 
+def recoup_dif():
+    result = []
+    for rec, degen in zip(ticks, ticks_degen):
+        result.append(rec -degen)
+
+    return result
+
+
+
 
 #print(ticks)
 
 
-def compute_cumulative_net_gain_per_tick(damage_interval=0.198, damage_amount=3000, recoup_percentage=0.7, total_duration=3.0, tick_duration=0.033):
+def compute_cumulative_net_gain_per_tick(damage_interval=0.198, damage_amount=3280, recoup_percentage=1.2, total_duration=3.0, tick_duration=0.033, life = 3000):
     ticks = compute_ticks(damage_interval, damage_amount, recoup_percentage, total_duration, tick_duration)
 
     net_gains = []
     cumulative_gain = 0
+    leech = life * 0.4 / 30.3
     for i, recoup in enumerate(ticks):
         current_time = i * tick_duration
 
         # Jeśli to tick z obrażeniami
         if current_time % damage_interval < tick_duration or current_time % damage_interval == 0:
-            damage_this_tick = 554
+            damage_this_tick = damage_amount * 0.1375
         else:
             damage_this_tick = 0
 
-        net_gain = recoup - damage_this_tick
+        net_gain = recoup - damage_this_tick - ticks_degen[i] + leech
         cumulative_gain += net_gain
         net_gains.append(cumulative_gain)
 
@@ -140,9 +171,11 @@ def compute_cumulative_net_gain_per_tick(damage_interval=0.198, damage_amount=30
 
 
 
-def find_lowest_cumulative_net_gain(damage_interval=0.198, damage_amount=2400, recoup_percentage=3, total_duration=3.0, tick_duration=0.033):
+def find_lowest_cumulative_net_gain(damage_interval=0.23, damage_amount=3280, recoup_percentage=1.2, total_duration=3.0,
+                                    tick_duration=0.033, progenesis = 40, petri= 45, life = 2700):
     ticks = compute_ticks(damage_interval, damage_amount, recoup_percentage, total_duration, tick_duration)
-
+    ticks_degen = compute_ticks_degen(damage_interval, damage_amount, total_duration, tick_duration, progenesis, petri)
+    leech = life * 0.4 / 30.3
     net_gains = []
     cumulative_gain = 0
     for i, recoup in enumerate(ticks):
@@ -150,11 +183,11 @@ def find_lowest_cumulative_net_gain(damage_interval=0.198, damage_amount=2400, r
 
         # Jeśli to tick z obrażeniami
         if current_time % damage_interval < tick_duration or current_time % damage_interval == 0:
-            damage_this_tick = 1200
+            damage_this_tick = damage_amount * (1 - petri/100) * (1 -  progenesis / 100)
         else:
             damage_this_tick = 0
 
-        net_gain = recoup - damage_this_tick
+        net_gain = recoup - damage_this_tick - ticks_degen[i] + leech
         cumulative_gain += net_gain
         net_gains.append(cumulative_gain)
 
@@ -168,3 +201,6 @@ lowest_value, tick_at_lowest = find_lowest_cumulative_net_gain()
 print(f"Najniższa wartość to: {lowest_value:.2f} HP, która wystąpiła w ticku {tick_at_lowest}.")
 
 
+#print(recoup_dif()[25])
+
+#print(sum(ticks_degen[:25]))
